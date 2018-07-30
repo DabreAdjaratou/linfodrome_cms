9bb379a7954b43f27a47f1ab46af9519ed524638
DabreAdjaratou <adjaratoudabre22@gmail.com>
2018-07-30 12:09:14 +0000

    change the main text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Linfodrome CMS
                 </div>
 
                 <div class="links">
